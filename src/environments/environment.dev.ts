af50ddaf3d166c15fc6451650c2a9b742d315f9d
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2021-11-04 08:19:49 +0530

    Stage api update and comments

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,3 +1,14 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Dev environment configurations
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2021-04-21 12:04:48 
+ * Last modified  : 2021-11-04 08:19:28
+ */
+
 export const environment = {
   production: true,
   apiEndpoint: 'https://c2.dev.api.rollingarray.co.in/'
--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -1,4 +1,15 @@
+/**
+ * © Rolling Array https://rollingarray.co.in/
+ *
+ *
+ * @summary Stage environment configurations
+ * @author code@rollingarray.co.in
+ *
+ * Created at     : 2021-04-21 12:04:48 
+ * Last modified  : 2021-11-04 08:19:06
+ */
+
 export const environment = {
   production: true,
-  apiEndpoint: 'https://c2-stage.api.rollingarray.co.in/'
+  apiEndpoint: 'https://c2.stage.api.rollingarray.co.in/'
 };

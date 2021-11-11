f6e3fec0079f0e3afedfc4056688e32e9f484f70
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2021-11-11 16:43:28 +0530

    Add pager to intro

--- src/app/shared/constant/array.constant.ts
+++ src/app/shared/constant/array.constant.ts
@@ -6,12 +6,13 @@
  * @author code@rollingarray.co.in
  *
  * Created at     : 2021-05-18 19:15:29 
- * Last modified  : 2021-11-06 21:40:53
+ * Last modified  : 2021-11-11 16:36:58
  */
 
 
 import { StringKey } from 'src/app/shared/constant/string.constant';
 import { RouteModel } from '../model/route.model';
+import { SlideModel } from '../model/slide.model';
 
 export class ArrayKey {
 	/**
@@ -70,6 +71,67 @@ export class ArrayKey {
 		},
 	];
 
+
+	/**
+	 * Intro slide array of array key
+	 */
+	public static readonly INTRO_SLIDE_ARRAY: SlideModel[] = [
+		{
+			title: StringKey.INTRO_SLIDE_0_TITLE,
+			info: StringKey.INTRO_SLIDE_0_INFO,
+			imageName: "inclusion",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_1_TITLE,
+			info: StringKey.INTRO_SLIDE_1_INFO,
+			imageName: "no_project",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_2_TITLE,
+			info: StringKey.INTRO_SLIDE_2_INFO,
+			imageName: "no_member",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_3_TITLE,
+			info: StringKey.INTRO_SLIDE_3_INFO,
+			imageName: "no_sprint",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_4_TITLE,
+			info: StringKey.INTRO_SLIDE_4_INFO,
+			imageName: "no_goal",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_5_TITLE,
+			info: StringKey.INTRO_SLIDE_5_INFO,
+			imageName: "no_activity",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_6_TITLE,
+			info: StringKey.INTRO_SLIDE_6_INFO,
+			imageName: "no_reviewer",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_7_TITLE,
+			info: StringKey.INTRO_SLIDE_7_INFO,
+			imageName: "no_diversity",
+			buttonText: StringKey.SKIP_INTRO,
+		},
+		{
+			title: StringKey.INTRO_SLIDE_8_TITLE,
+			info: StringKey.INTRO_SLIDE_8_INFO,
+			imageName: "no_credibility",
+			buttonText: StringKey.GOT_IT_CONTINUE,
+		},
+	];
+	
 	/**
 	 * Next step of array key
 	 */

dc0b6158a8d1a4d2d8062c80b39137ebf0eefbed
Rolling Array <82917132+RollingArray@users.noreply.github.com>
2021-11-23 11:20:04 +0530

    Change choose action delete icon

--- src/app/pages/project-sprint/project-sprint.page.ts
+++ src/app/pages/project-sprint/project-sprint.page.ts
@@ -341,7 +341,7 @@ export class ProjectSprintPage extends BaseViewComponent implements OnInit, OnDe
 				},
 				{
 					text: this.stringKey.DELETE + ' ' + this.stringKey.DETAILS,
-					icon: this.stringKey.ICON_EDIT,
+					icon: this.stringKey.ICON_DELETE,
 					handler: () => {
 						this.deleteProjectSprint(selectedSprint);
 					}
